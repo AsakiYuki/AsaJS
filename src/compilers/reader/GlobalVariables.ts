5ed9c0947aef21d2601604aa0a996948f61f1261
Asaki Yuki <vantrong2007vn@gmail.com>
2025-05-08 21:14:17 +0700

    fix global variables file is not workig

--- src/compilers/reader/GlobalVariables.ts
+++ src/compilers/reader/GlobalVariables.ts
@@ -2,8 +2,8 @@ import { Obj } from "./Object";
 import { JsonBuilder } from "../generator/JsonBuilder";
 import { ReadValue } from "./ReadProperties";
 
-Obj.forEach(require(`${process.cwd()}/asakiyuki.config.cjs`).global_variables, (key, value) =>
+Obj.forEach(require(`${process.cwd()}/asakiyuki.global_variables.cjs`).global_variables, (key, value) =>
     JsonBuilder.registerGlobalVariable(key, ReadValue(value))
 );
 
-export {};
+export { };
